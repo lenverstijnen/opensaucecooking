9deb6c13fcfd660fae20ac023b318375118ddfba
Jesse Buitenhuis <jessebuitenhuis@gmail.com>
2021-02-06 16:15:25 +0100

    auth in backend

--- server/src/startUp/startRoutes.ts
+++ server/src/startUp/startRoutes.ts
@@ -1,13 +1,8 @@
-import { Express } from "express"
-import bodyParser from "body-parser"
-import cors from "cors"
+import { Express } from "express";
 
 // Routes
-import recipe from "../routes/recipe"
+import recipe from "../routes/recipe";
 
 export const startRoutes = (app: Express) => {
-  app.use(bodyParser.json())
-  app.use(cors())
-
-  app.use("/api/recipe/", recipe)
-}
+  app.use("/api/recipe/", recipe);
+};
