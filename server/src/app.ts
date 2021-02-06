5872ec8bd2d7e881575cb08dfea6b1dab8e6d2bb
Len Verstijnen <lenverstijnen@me.com>
2021-02-06 15:28:17 +0100

    Add startDb

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
 import dotenv from "dotenv"
 dotenv.config()
 import express from "express"
+import startDb from "./startUp/startDb"
 import { startRoutes } from "./startUp/startRoutes"
 const app = express()
 
+startDb()
 startRoutes(app)
 
 export default app
---
+++ server/src/startUp/startDb.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+
+mongoose.set("useUnifiedTopology", true)
+mongoose.set("useNewUrlParser", true)
+mongoose.set("useFindAndModify", false)
+mongoose.set("useCreateIndex", true)
+
+const dbUri = process.env.DB_URI
+
+export default () => {
+  try {
+    mongoose.connect(dbUri as string)
+    console.log("Connected to MongoDb")
+  } catch (error) {
+    console.log(error)
+  }
+}
