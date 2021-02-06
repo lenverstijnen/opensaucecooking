067cec8f1e1885a638a7be7b508aa92c5c236784
Jesse Buitenhuis <jessebuitenhuis@gmail.com>
2021-02-06 16:15:18 +0100

    auth in backend

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,14 @@
-import dotenv from "dotenv"
-dotenv.config()
-import express from "express"
-import { startRoutes } from "./startUp/startRoutes"
-const app = express()
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import { startAuth } from "./startUp/startAuth";
+import { startMiddleware } from "./startUp/startMiddleware";
+import { startRoutes } from "./startUp/startRoutes";
 
-startRoutes(app)
+const app = express();
 
-export default app
+startMiddleware(app);
+startAuth(app);
+startRoutes(app);
+
+export default app;
