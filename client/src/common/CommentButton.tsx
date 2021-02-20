5a7eba2abbbdb0c658747c7d645507c1865a893c
Jesse Buitenhuis <jessebuitenhuis@gmail.com>
2021-02-20 17:45:35 +0100

    buttons

---
+++ client/src/common/CommentButton.tsx
@@ -0,0 +1,10 @@
+import { IconButton } from "@material-ui/core";
+import { ChatBubbleOutline } from "@material-ui/icons";
+
+export const CommentButton = () => {
+  return (
+    <IconButton>
+      <ChatBubbleOutline></ChatBubbleOutline>
+    </IconButton>
+  );
+};
