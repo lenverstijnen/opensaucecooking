0dde03d4c398add99f0cb6a9599a0193ebda68de
Len Verstijnen <lenverstijnen@me.com>
2021-02-21 21:48:11 +0100

    Change text color app header links

--- client/src/app-header/AppHeader.tsx
+++ client/src/app-header/AppHeader.tsx
@@ -1,15 +1,21 @@
-import { AppBar, makeStyles, Toolbar, Typography } from "@material-ui/core";
-import { AuthButton } from "./AuthButton";
-import { Link } from "react-router-dom";
+import { AppBar, makeStyles, Toolbar, Typography } from "@material-ui/core"
+import { AuthButton } from "./AuthButton"
+import { Link } from "react-router-dom"
 
 const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
-}));
+  link: {
+    textDecoration: "none",
+    textTransform: "capitalize",
+    color: "white",
+    margin: "0 10px",
+  },
+}))
 
 export const AppHeader = () => {
-  const classes = useStyles();
+  const classes = useStyles()
 
   return (
     <AppBar position="sticky">
@@ -17,10 +23,14 @@ export const AppHeader = () => {
         <Typography variant="h6" className={classes.title}>
           Open Sauce Cooking
         </Typography>
-        <Link to="/recipes">Recipes</Link>
-        <Link to="/recipes/create">Create recipe</Link>
+        <Link className={classes.link} to="/recipes">
+          Recipes
+        </Link>
+        <Link className={classes.link} to="/recipes/create">
+          Create recipe
+        </Link>
         <AuthButton />
       </Toolbar>
     </AppBar>
-  );
-};
+  )
+}
